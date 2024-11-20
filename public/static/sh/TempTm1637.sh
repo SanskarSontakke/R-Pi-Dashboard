6b08649f990baafb81695fb97fef0fb83287f714
sanskar <sanskarsontakke@gmail.com>
2024-11-20 12:01:35 +0530

    Update

--- public/static/sh/TempTm1637.sh
+++ public/static/sh/TempTm1637.sh
@@ -1,7 +1,7 @@
 
 
 while true; do
-  cd Dashboard/Raspberry-tm1637/
+  cd ~/Dashboard/Raspberry-tm1637/
   temp=$(vcgencmd measure_temp | grep -oP '\d+' | head -n 1)
   temp_display="${temp} C"
   ./tm1637 -t "$temp_display"
